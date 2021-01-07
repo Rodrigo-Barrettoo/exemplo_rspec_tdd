1fce7d5f0be52c9917fe7c0c3462c85f76248d7a
Rodrigo-Barrettoo <rodrigonarrettoo@outlook.com>
2021-01-07 13:49:20 -0300

    Negativando Matchers

--- spec/matchers/arrays_spec.rb
+++ spec/matchers/arrays_spec.rb
@@ -1,7 +1,10 @@
+RSpec::Matchers.define_negated_matcher :exclude, :include
+
 describe Array.new([1,2,3]), 'Matchers para Arrays' do
   it '#include' do
     expect(subject).to include(2)
     expect(subject).to include(1,2)
+    expect(subject).to exclude(4)
   end
 
   it '#match_array' do
--- spec/matchers/be_within_spec.rb
+++ spec/matchers/be_within_spec.rb
@@ -1,5 +1,9 @@
+RSpec::Matchers.define_negated_matcher :be_not_within, :be_within
+
+
 describe 'be_within' do
   it { expect(12.5).to be_within(0.5).of(12) }
+  it { expect(11.4).to be_not_within(0.5).of(12) }
 end
 
 # delta 0.5
