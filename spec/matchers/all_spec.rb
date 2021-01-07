7c699aa21fcbdd883878045a67586f5d5f173090
Rodrigo-Barrettoo <rodrigonarrettoo@outlook.com>
2021-01-07 15:46:52 -0300

    Tag Filters

--- spec/matchers/all_spec.rb
+++ spec/matchers/all_spec.rb
@@ -1,4 +1,4 @@
-describe 'All' do
+describe 'All', type: 'collection' do
   it { expect([1,5,9]).to all( be_odd().and be_an(Integer)) }
   it { expect(['ruby, rails']).to all( be_a(String).and include('r')) }
 end
--- spec/matchers/arrays_spec.rb
+++ spec/matchers/arrays_spec.rb
@@ -1,13 +1,13 @@
 RSpec::Matchers.define_negated_matcher :exclude, :include
 
-describe Array.new([1,2,3]), 'Matchers para Arrays' do
+describe Array.new([1,2,3]), 'Matchers para Arrays', type: 'collection' do
   it '#include' do
     expect(subject).to include(2)
     expect(subject).to include(1,2)
     expect(subject).to exclude(4)
   end
 
-  it '#match_array' do
+  it '#match_array', slow: true do
     expect(subject).to match_array([1,2,3])
   end
 
