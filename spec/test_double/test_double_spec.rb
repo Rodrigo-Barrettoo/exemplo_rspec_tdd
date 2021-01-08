4cbe446d14141413cb445c2dd46de26fcc63efbb
Rodrigo-Barrettoo <rodrigonarrettoo@outlook.com>
2021-01-08 15:02:59 -0300

    Método "As Null Object"

--- spec/test_double/test_double_spec.rb
+++ spec/test_double/test_double_spec.rb
@@ -7,4 +7,14 @@ describe 'Test Double' do
     puts user.name
     puts user.password
   end
+
+  it 'as_null_object' do
+    user = double('User').as_null_object
+
+    allow(user).to receive_messages(name: 'Barretin', password: 'secret')
+
+    puts user.name
+    puts user.password
+    user.abc
+  end
 end
